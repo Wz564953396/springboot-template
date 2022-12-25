15295f754da229374daf0f957d2ca1dc240f30fd
zhao.wang <564953396@qq.com>
2022-12-25 17:15:20 +0800

    spring-security - 增加注销登陆功能

--- src/main/java/com/wz/example/template/config/SecurityConfiguration.java
+++ src/main/java/com/wz/example/template/config/SecurityConfiguration.java
@@ -1,9 +1,12 @@
 package com.wz.example.template.config;
 
+import com.wz.example.template.security.MyAuthenticationFailureHandler;
 import com.wz.example.template.security.MyAuthenticationSuccessHandler;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpRequest;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -11,6 +14,9 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.rememberme.JdbcTokenRepositoryImpl;
 import org.springframework.security.web.authentication.rememberme.PersistentTokenRepository;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.security.web.util.matcher.OrRequestMatcher;
+import org.springframework.security.web.util.matcher.RequestMatcher;
 
 import javax.sql.DataSource;
 
@@ -27,6 +33,9 @@ public class SecurityConfiguration {
 
     @Autowired
     private MyAuthenticationSuccessHandler myAuthenticationSuccessHandler;
+
+    @Autowired
+    private MyAuthenticationFailureHandler myAuthenticationFailureHandler;
 
 //    默认是token保存在内存中，手动注入JdbcTokenRepositoryImpl，实现将token保存在数据库中，同时自动建表
     @Bean
@@ -45,17 +54,29 @@ public class SecurityConfiguration {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         return http.csrf().disable()                                 //关闭CSRF防护，不知道干嘛用的
-                .formLogin()                                  //自定义登录页面
-                .loginPage("/login.html")                     //登录页面路径设置
-                .loginProcessingUrl("/user/login")            //定义登录用户名密码提交的接口路径
-                .defaultSuccessUrl("/index.html")             //登录成功后，默认跳转路径
-                .successHandler(myAuthenticationSuccessHandler)//前后端分离情况下，不需要返回静态资源，采用handler方式返回restful数据
+                .formLogin()                                    //自定义登录页面
+                .loginPage("/login.html")                       //登录页面路径设置
+                .loginProcessingUrl("/user/login")              //定义登录用户名密码提交的接口路径
+                .defaultSuccessUrl("/index.html")               //登录成功后，默认跳转路径
+                .successHandler(myAuthenticationSuccessHandler) //前后端分离情况下，不需要返回静态资源，采用handler方式返回restful数据
+//                .failureForwardUrl("/login.html")               //认证失败后，forward（转发）到
+//                .failureUrl("/login.html")                      //认证失败后，redirect（重定向）到
+                .failureHandler(myAuthenticationFailureHandler) //前后端分离情况下，不需要返回静态资源，认证失败后返回restful数据
+
+                .and()
+                .logout()                                       //拿到注销登录的配置对象，注销登录功能默认是开启的
+                .logoutUrl("/user/logout")                      //指定注销登录的url，默认值为"/logout"，请求方式为GET，Security 5.7.2之后，默认GET,POST,PUT,DELETE请求方式都可以
+                .invalidateHttpSession(true)                    //使当前会话失效，默认为true
+                .clearAuthentication(true)                      //清空当前认证标记，默认为true
+                .logoutSuccessUrl("/login.html")                //注销登录后，跳转到某个url上，默认为"/login?logout"
+                .logoutRequestMatcher(new OrRequestMatcher(
+                        new AntPathRequestMatcher("/user/logout", HttpMethod.POST.name()),
+                        new AntPathRequestMatcher("", HttpMethod.GET.name())))
+                                                                //自定义注销登录的请求路径和请求方式
+                .permitAll()
 
 //                设置拒绝访问页面
                 .and().exceptionHandling().accessDeniedPage("/403.html")
-
-                .and().logout().logoutUrl("/user/logout")      //用户注销，设置退出接口
-                .logoutSuccessUrl("/logout.html").permitAll()   //用户退出后的访问页面
 
 //                设置哪些路径可以直接访问，不需要安全认证
                 .and().authorizeRequests().antMatchers(permitPaths).permitAll()
